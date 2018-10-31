e4cd5b2d83778d8ff6eac4e4e282da1943337b06
chirag0703 <44295434+chirag0703@users.noreply.github.com>
2018-10-31 16:49:25 +0100

    Add files via upload

---
+++ Image.java
@@ -0,0 +1,49 @@
+import java.io.FileOutputStr;
+import java.io.IOException;
+import java.io.*;
+
+public class Image {
+  public byte[] imData = null ;
+  private int w = 0;
+  private int h = 0;
+
+  public Image(int w, int h) {
+    this.w = wide;
+    this.h = high;
+
+    int nOfPix = wide * high;
+    int nOfBytes = nOfPix * 3 ;
+    this.imData = new byte[nOfBytes];
+  }
+
+  public void set(int x, int y, int val) {
+    if (x > this.wide && y > this.high) return;
+    int positionFirstIndex = (this.wide * y + x) * 3;
+    this.imData[positionFirstIndex ] = (byte)(val >> 16);
+    this.imData[positionFirstIndex + 1] = (byte)(val >> 8);
+    this.imData[positionFirstIndex + 2] = (byte)(val >> 0);
+  }
+
+  // --- write image data to file as image format P6
+  
+    public void  write( String filename) throws IOException{
+    BufferedWriter bw = new BufferedWriter(new OutputStrWriter(new FileOutputStr(filename)));
+    int rdim = width;
+    int cdim = height;
+    bw.write("P6");
+    bw.newLine();
+    bw,write(width+" "+height);
+    bw.newLine();
+    bw.write("255");
+    bw.newLine();
+    for(int row=0;row<rdime;row++){
+      for(int column=0;column<cdim;column++){
+        bw.write(String.valueOf(imData));
+        if(column < cdim - 1)bw.write(" ");
+      }
+      bw.newLine();
+    }
+    bw.flush();
+    bw.close();
+  }
+}
--- TestSuite.java
+++ TestSuite.java
@@ -5,23 +5,27 @@ public class TestSuite {
 
   @Test public static void ImageTest1() {
     Image i = new Image(100,100);
-    assert (i.data.length == 30000);
+    assert (i.imData.length == 30000);
   }
 
   @Test public static void ImageTest2() {
     Image i = new Image(100,100);
     i.set(0,0,0x123456);
-    assert (i.data[0] == (byte)0x12);
-    assert (i.data[1] == (byte)0x34);
-    assert (i.data[2] == (byte)0x56);
-    assert (i.data[3] == (byte)0x00);
+    assert (i.imData[0] == (byte)0x12);
+    assert (i.imData[1] == (byte)0x34);
+    assert (i.imData[2] == (byte)0x56);
+    assert (i.imData[3] == (byte)0x00);
   }
 
   @Test public static void ImageTest3() {
     Image i = new Image(100,100);
     i.set(99,99,0x123456);
-    int len = i.data.length;
-    assert (false);
+    assert (i.imData[29995] == (byte)0x00);
+    assert (i.imData[29996] == (byte)0x00);
+    // the first byte in pixel has index = (99 * 100 + 99) * 3 = 29997
+    assert (i.imData[29997] == (byte)0x12);
+    assert (i.imData[29998] == (byte)0x34);
+    assert (i.imData[29999] == (byte)0x56);
   }
 
   @Test public static void ImageTest4() throws java.io.FileNotFoundException,java.io.IOException {
@@ -32,5 +36,4 @@ public class TestSuite {
     boolean exists = f.exists() && f.isFile();
     assert (exists);
   }
-
 }
